d8b5d701269fc4560c9a66ac2a42043bf0325b09
LordSquishers <lordsquisherton@gmail.com>
2019-10-08 17:46:24 +0100

    included the excluded

---
+++ src/underground/shaders/block/blockGS.glsl
@@ -0,0 +1,103 @@
+#version 150
+
+layout (points) in;
+layout (triangle_strip, max_vertices = 24) out;
+
+in vec2 _texCoords[];
+in vec3 toLightVector[];
+in vec3 toCameraVector[];
+in float visibility[];
+
+out vec2 texCoords;
+out vec3 surfaceNormal;
+
+out vec3 _toLightVector;
+out vec3 _toCameraVector;
+out float _visibility;
+
+uniform bool sides[6];
+
+void createVertex(vec3 offset, vec3 normal){
+    vec4 actualOffset = vec4(offset * 0.5, 0.0);
+    vec4 worldPosition = (gl_in[0].gl_Position + actualOffset);
+    gl_Position = worldPosition;
+
+    texCoords = _texCoords[0];
+    _toLightVector = toLightVector[0];
+    _toCameraVector = toCameraVector[0];
+    _visibility = visibility[0];
+    surfaceNormal = normal;
+
+    EmitVertex();
+}
+
+void main(void) {
+
+  // front
+  vec3 faceNormal;
+
+  if(sides[4]) {
+      faceNormal = vec3(0.0, 0.0, 1.0);
+      createVertex(vec3(-1.0, 1.0, 1.0), faceNormal);
+      createVertex(vec3(-1.0, -1.0, 1.0), faceNormal);
+      createVertex(vec3(1.0, 1.0, 1.0), faceNormal);
+      createVertex(vec3(1.0, -1.0, 1.0), faceNormal);
+
+      EndPrimitive();
+  }
+
+  // right
+  if(sides[3]) {
+      faceNormal = vec3(1.0, 0.0, 0.0);
+      createVertex(vec3(1.0, 1.0, 1.0), faceNormal);
+      createVertex(vec3(1.0, -1.0, 1.0), faceNormal);
+      createVertex(vec3(1.0, 1.0, -1.0), faceNormal);
+      createVertex(vec3(1.0, -1.0, -1.0), faceNormal);
+
+      EndPrimitive();
+  }
+
+  // back
+  if(sides[5]) {
+      faceNormal = vec3(0.0, 0.0, -1.0);
+      createVertex(vec3(1.0, 1.0, -1.0), faceNormal);
+      createVertex(vec3(1.0, -1.0, -1.0), faceNormal);
+      createVertex(vec3(-1.0, 1.0, -1.0), faceNormal);
+      createVertex(vec3(-1.0, -1.0, -1.0), faceNormal);
+
+      EndPrimitive();
+  }
+
+  // left
+  if(sides[2]) {
+      faceNormal = vec3(-1.0, 0.0, 0.0);
+      createVertex(vec3(-1.0, 1.0, -1.0), faceNormal);
+      createVertex(vec3(-1.0, -1.0, -1.0), faceNormal);
+      createVertex(vec3(-1.0, 1.0, 1.0), faceNormal);
+      createVertex(vec3(-1.0, -1.0, 1.0), faceNormal);
+
+      EndPrimitive();
+  }
+
+  // top
+  if(sides[0]) {
+      faceNormal = vec3(0.0, 1.0, 0.0);
+      createVertex(vec3(1.0, 1.0, 1.0), faceNormal);
+      createVertex(vec3(1.0, 1.0, -1.0), faceNormal);
+      createVertex(vec3(-1.0, 1.0, 1.0), faceNormal);
+      createVertex(vec3(-1.0, 1.0, -1.0), faceNormal);
+
+      EndPrimitive();
+  }
+
+  // bottom
+  if(sides[1]) {
+      faceNormal = vec3(0.0, -1.0, 0.0);
+      createVertex(vec3(-1.0, -1.0, 1.0), faceNormal);
+      createVertex(vec3(-1.0, -1.0, -1.0), faceNormal);
+      createVertex(vec3(1.0, -1.0, 1.0), faceNormal);
+      createVertex(vec3(1.0, -1.0, -1.0), faceNormal);
+
+      EndPrimitive();
+  }
+}
---
+++ src/underground/shaders/block/blockVS.glsl
@@ -0,0 +1,35 @@
+#version 150
+
+in vec3 position;
+in vec2 texCoords;
+
+out vec2 _texCoords;
+out vec3 toLightVector;
+out vec3 toCameraVector;
+out float visibility;
+
+uniform vec3 lightPos;
+
+uniform mat4 transformMat;
+uniform mat4 projectionMat;
+uniform mat4 viewMat;
+
+const float density = 0.0035;
+const float gradient = 5.0;
+
+void main(void) {
+
+  vec4 worldPos = transformMat * vec4(position, 1.0);
+  vec4 positionRelativeToCam = viewMat * worldPos;
+  gl_Position = projectionMat * positionRelativeToCam;
+
+  _texCoords = texCoords;
+
+  toLightVector = lightPos - worldPos.xyz;
+  toCameraVector = (inverse(viewMat) * vec4(0.0, 0.0, 0.0, 1.0)).xyz - worldPos.xyz;
+
+  float distance = length(positionRelativeToCam.xyz);
+  visibility = exp(-pow((distance * density), gradient));
+  visibility = clamp(visibility, 0.0, 1.0);
+
+}
